992256e4c3299693c7af6171e5a2e4cbe50a1a82
Курванов Закиржан <@itstep.loc>
2022-05-10 16:55:56 +0600

    Добавьте файлы проекта.

---
+++ WpfApp2/Model/DataSettings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2.Model
+{
+    class DataSettings
+    {
+        MCSEntities db = new MCSEntities();
+
+        public List<newEquipment> getListOfCars()
+        {
+            List<newEquipment> newEquipments = new List<newEquipment>();
+            newEquipments = db.newEquipment.ToList();
+            return newEquipments;
+        }
+     }
+}
